1baa1a4af199cc448cce469400d029667298b820
Jacob Gildenblat <jacob.gildenblat@gmail.com>
2024-03-28 19:26:23 +0200

    avg mz visualization

--- msi_visual/avgmz_nmf_segmentation.py
+++ msi_visual/avgmz_nmf_segmentation.py
@@ -2,18 +2,21 @@ import cv2
 import cmapy
 from PIL import Image
 import numpy as np
+from msi_visual.utils import normalize_image_grayscale, image_histogram_equalization
 
-from msi_visual.utils import brain_nmf_semantic_segmentation, normalize_image_grayscale, image_histogram_equalization
-
-class SegmentationUMAPVisualization:
-    def __init__(self, umap_model, segmentation_model):
-        self.umap_model = umap_model
+class SegmentationAvgMZVisualization:
+    def __init__(self, segmentation_model):
         self.segmentation_model = segmentation_model
         self.k = self.segmentation_model.k
 
     def factorize(self, img):
-        self.umap_1d = self.umap_model.predict(img)[:, :, 0]
+        processed_img = img / img.sum(axis=-1)[:, :, None]
+
         self.segmentation = self.segmentation_model.factorize(img)
+
+        mz = np.arange(processed_img.shape[-1])
+        mz = mz[None, None, :]
+        self.avgmz = (processed_img * mz).mean(axis=-1)
         return self.segmentation
 
     def get_subsegmentation(self, img, roi_mask, segmentation, number_of_bins_for_comparison):
@@ -22,24 +25,24 @@ class SegmentationUMAPVisualization:
         regions = np.unique(segmentation_argmax)
         num_regions = len(regions)
         sub_segmentation = np.zeros(shape=segmentation_argmax.shape[:2], dtype=np.int32)
-        region_umaps = np.zeros(shape=segmentation_argmax.shape, dtype=np.float32)
+        region_heatmaps = np.zeros(shape=segmentation_argmax.shape, dtype=np.float32)
 
         for region in regions:
             region_mask = np.uint8(segmentation_argmax == region) * 255
             region_mask[roi_mask == 0] = 0
-            region_umap = self.umap_1d.copy()
-            region_umap[region_mask == 0] = 0
+            region_heatmap = self.avgmz.copy()
+            region_heatmap[region_mask == 0] = 0
 
-            region_umap = normalize_image_grayscale(region_umap, high_percentile=99)
+            region_heatmap = normalize_image_grayscale(region_heatmap, high_percentile=99)
             num_bins = 2048
-            region_umap = image_histogram_equalization(region_umap, region_mask, num_bins) / (num_bins - 1)
-            region_umaps[region_mask > 0] = region_umap[region_mask > 0]
+            region_heatmap = image_histogram_equalization(region_heatmap, region_mask, num_bins) / (num_bins - 1)
+            region_heatmaps[region_mask > 0] = region_heatmap[region_mask > 0]
 
             bins = np.linspace(0, 1, number_of_bins_for_comparison)
 
-            digitized = np.digitize(region_umap, bins)
+            digitized = np.digitize(region_heatmap, bins)
             sub_segmentation[region_mask > 0] = digitized[region_mask > 0] + (number_of_bins_for_comparison + 2) * region
-        return sub_segmentation, region_umaps
+        return sub_segmentation, region_heatmaps
 
     def visualize_factorization(self,
                                 img,
