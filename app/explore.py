503e97a06220a93ca69ab27f7b45a23b467ebbdc
Jacob Gildenblat <jacob.gildenblat@gmail.com>
2024-06-11 11:15:14 +0300

    exploration tool

---
+++ app/explore.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import glob
+import os
+import json
+import sys
+import numpy as np
+import joblib
+from collections import defaultdict
+from pathlib import Path
+from argparse import Namespace
+from PIL import Image
+from st_pages import show_pages_from_config, add_page_title
+from msi_visual.percentile_ratio import percentile_ratio_rgb
+from msi_visual import nmf_3d
+from msi_visual import parametric_umap
+from msi_visual.app_utils.extraction_info import display_paths_to_extraction_paths, \
+    get_files_from_folder
+from keras.callbacks import Callback
+
+# Either this or add_indentation() MUST be called on each page in your
+# app to add indendation in the sidebar
+add_page_title()
+
+show_pages_from_config()
+
+if 'pr' not in st.session_state:
+    st.session_state.pr = {}
+
+regions = []
+with st.sidebar:
+    extraction_root_folder = st.text_input("Extraction Root Folder")
+
+    if extraction_root_folder:
+        extraction_folders = display_paths_to_extraction_paths(extraction_root_folder)
+
+        selected_extraction = st.selectbox('Extraction folder', extraction_folders.keys())
+        if selected_extraction:
+            extraction_folder = extraction_folders[selected_extraction]
+            paths=get_files_from_folder(extraction_folder)
+            regions = st.multiselect('Regions to include', paths, paths)
+
+
+for path in regions:
+    if path in st.session_state.pr:
+        pr = st.session_state.pr[path]
+    else:
+        with st.spinner(text=f"Generating High Saliency Percentile-Ratio Visualization for {path}.."):
+            img = np.load(path)
+            pr = percentile_ratio_rgb(img)
+    
+    st.text(path)
+    st.image(pr)
