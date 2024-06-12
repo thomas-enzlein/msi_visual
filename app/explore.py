e62323e1025afe17b097f133e9a4f81ec2e9cf4a
Jacob Gildenblat <jacob.gildenblat@gmail.com>
2024-06-12 18:00:15 +0300

    Expose PR parameters in dataset tool

--- app/explore.py
+++ app/explore.py
@@ -10,12 +10,14 @@ from pathlib import Path
 from argparse import Namespace
 from PIL import Image
 from st_pages import show_pages_from_config, add_page_title
+import importlib
+import msi_visual.percentile_ratio
+importlib.reload(msi_visual.percentile_ratio)
 from msi_visual.percentile_ratio import percentile_ratio_rgb
 from msi_visual import nmf_3d
 from msi_visual import parametric_umap
 from msi_visual.app_utils.extraction_info import display_paths_to_extraction_paths, \
     get_files_from_folder
-from keras.callbacks import Callback
 
 def save_data(path=None):
     folder = "percentile_ratio_images"
@@ -52,16 +54,32 @@ with st.sidebar:
             regions = st.multiselect('Regions to include', paths, paths)
 
 
-for path in regions:
-    if path in st.session_state.pr:
-        pr = st.session_state.pr[path]
-    else:
-        with st.spinner(text=f"Generating High Saliency Percentile-Ratio Visualization for {path}.."):
-            img = np.load(path)
-            pr = percentile_ratio_rgb(img)
-            st.session_state.pr[path] = pr
-    
-    st.text(path)
-    st.image(pr)
+cols = st.columns(6)
+st.text("Will be computed as Percentile(a)/Percentile(b), Percentile(c)/Percentile(d), Percentile(e)/Percentile(f)")
+p0 = cols[0].number_input("a", min_value=0.0, max_value=100.0, value=99.99)
+p1 = cols[1].number_input("b", min_value=0.0, max_value=100.0, value=99.9)
+p2 = cols[2].number_input("c", min_value=0.0, max_value=100.0, value=99.9)
+p3 = cols[3].number_input("d", min_value=0.0, max_value=100.0, value=99.0)
+p4 = cols[4].number_input("e", min_value=0.0, max_value=100.0, value=98.0)
+p5 = cols[5].number_input("f", min_value=0.0, max_value=100.0, value=85.0)
 
-    save_data(path=path)
+percentiles = [p0, p1, p2, p3, p4, p5]
+
+equalize = st.checkbox("Equalize histogram internally")
+
+settings_str = "".join([f"{p:.3f}" for p in percentiles]) + str(equalize)
+
+if st.button("Run"):
+    for path in regions:
+        if path+settings_str in st.session_state.pr:
+            pr = st.session_state.pr[path+settings_str]
+        else:
+            with st.spinner(text=f"Generating High Saliency Percentile-Ratio Visualization for {path}.."):
+                img = np.load(path)
+                pr = percentile_ratio_rgb(img, percentiles=percentiles, equalize=equalize)
+                st.session_state.pr[path + settings_str] = pr
+        
+        st.text(path)
+        st.image(pr)
+
+        save_data(path=path+settings_str)
--- msi_visual/percentile_ratio.py
+++ msi_visual/percentile_ratio.py
@@ -3,34 +3,47 @@ import cv2
 
 from msi_visual.normalization import spatial_total_ion_count, total_ion_count, median_ion
 
-def percentile_ratio_rgb(img, normalization='tic'):
+def percentile_ratio_rgb(img,
+                         normalization='tic',
+                         percentiles = [99.99, 99.9, 99.9, 99, 98, 85], equalize=False):
     norm_funtion = {'tic': total_ion_count, 'median': median_ion, 'spatial_tic': spatial_total_ion_count}[normalization]
     normalized = norm_funtion(img)
 
     sorted_normalized = np.sort(normalized, axis=-1)
     N = sorted_normalized.shape[-1]
 
-    p_9999 = sorted_normalized[:, :, int(N*99.99/100)]
-    p_999 = sorted_normalized[:, :, int(N*99.9/100)]
-    p_99 = sorted_normalized[:, :, int(N*99/100)]
-    p_98 = sorted_normalized[:, :, int(N*98/100)]
-    p_85 = sorted_normalized[:, :, int(N*85/100)]
+
+    p0 = sorted_normalized[:, :, int(N*percentiles[0]/100)]
+    p1 = sorted_normalized[:, :, int(N*percentiles[1]/100)]
+    p2 = sorted_normalized[:, :, int(N*percentiles[2]/100)]
+    p3 = sorted_normalized[:, :, int(N*percentiles[3]/100)]
+    p4 = sorted_normalized[:, :, int(N*percentiles[4]/100)]
+    p5 = sorted_normalized[:, :, int(N*percentiles[5]/100)]
 
 
-    a = p_9999 / (1e-5 + p_999)
+    a = p0 / (1e-5 + p1)
     a = a / np.percentile(a, 99.9)
     a[a > 1] = 1
 
-    b = p_999 / (1e-5 + p_99)
+    b = p2 / (1e-5 + p3)
     b = b / np.percentile(b, 99.9)
     b[b > 1] = 1
 
-    c = p_98 / (1e-5 + p_85)
+    c = p4 / (1e-5 + p5)
     c = c / np.percentile(c, 99.9)
     c[c > 1] = 1
 
     visualization = cv2.merge([(np.uint8(255*a)), (np.uint8(255*b)), (np.uint8(255*c))])
 
+
     visualization = cv2.cvtColor(visualization, cv2.COLOR_LAB2LRGB)
+
+    if equalize:
+        visualization = cv2.merge([cv2.equalizeHist(visualization[:, :, 0]),
+                                   cv2.equalizeHist(visualization[:, :, 1]),
+                                   cv2.equalizeHist(visualization[:, :, 2])])
+
+
+
     visualization[img.max(axis=-1) == 0] = 0
     return visualization
