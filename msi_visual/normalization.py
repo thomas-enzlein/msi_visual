e5ffef3e2efc63f9dccefb8153a1ce2b32c04fa5
Jacob Gildenblat <jacob.gildenblat@gmail.com>
2024-06-18 14:39:59 +0300

    1. Add input normalization 2. Statistics for point comparison using rank difference

--- msi_visual/normalization.py
+++ msi_visual/normalization.py
@@ -2,16 +2,16 @@
 import numpy as np
 import time
 
-def spatial_total_ion_count(img):
-    t0 = time.time()
-    processed = img / (1e-6 + np.sum(img, axis=-1)[:, :, None])
-    processed = processed / (1e-6 + np.percentile(processed, 99, axis=(0, 1))[None, None, :])
-    processed[processed > 1 ] = 1
-    print("norm took", time.time()-t0)
-    return img
-
 def total_ion_count(img):
     return img / (1e-6 + np.sum(img, axis=-1)[:, :, None])
 
 def median_ion(img):
     return img / (1e-6 + np.median(img, axis=-1)[:, :, None])
+
+def spatial_total_ion_count(img):
+    t0 = time.time()
+    processed = total_ion_count(img)
+    processed = processed / (1e-6 + np.percentile(processed, 99, axis=(0, 1))[None, None, :])
+    processed[processed > 1 ] = 1
+    print("norm took", time.time()-t0)
+    return img
