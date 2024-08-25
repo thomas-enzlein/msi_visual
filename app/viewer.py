4d0d19d242b5151280eef32d7ed8e049884a8afe
Jacob Gildenblat <jacob.gildenblat@gmail.com>
2024-08-25 20:16:54 +0300

    point to point comparisons

--- app/viewer.py
+++ app/viewer.py
@@ -7,7 +7,7 @@ import importlib
 import time
 import utils.viewer
 importlib.reload(utils.viewer)
-from utils.viewer import display_comparison, get_stats, get_raw_ion_image, create_ion_image, get_data, viewer, ClickData, region_with_all_comparisons, region_with_region_comparisons, show_ion_images
+from utils.viewer import display_comparison, get_stats, get_raw_ion_image, create_ion_image, get_data, viewer, ClickData, region_with_all_comparisons, region_with_region_comparisons, point_with_point_comparisons, show_ion_images
 import msi_visual.visualizations
 importlib.reload(msi_visual.visualizations)
 
@@ -28,7 +28,7 @@ with settings_tab:
     folder = st.text_input("Visualization folder")
     bins = st.number_input("Number of segmentation bins", min_value=5, step=1, value=5)
     equalize = st.checkbox('Equalize')
-    comparison = st.selectbox("Comparison type", ["Region with Region", "Region with all"], on_change=reset_clicks)
+    comparison = st.selectbox("Comparison type", ["Region with Region", "Region with all", "Point with Point"], on_change=reset_clicks)
     selection_type = st.selectbox("Selection Type", ["Click", "Polygon"], on_change=reset_clicks)
     stats_method = st.selectbox("Compairson method", ["U-Test", "Difference in ROI-MEAN"], on_change=reset_clicks)
 
@@ -40,8 +40,10 @@ with viz_tab:
 with region_tab:
     if comparison == "Region with all":
         region_with_all_comparisons(stats_method)
-    else:
+    elif comparison == "Region with Region":
         region_with_region_comparisons(stats_method)
+    elif comparison == "Point with Point":
+        point_with_point_comparisons(stats_method)
         
 with ion_tab:
     mzs_scores = st.text_input("mz values (comma separated)")
@@ -52,7 +54,3 @@ with ion_tab:
         except Exception as e:
             st.write(e)
             pass
-
-if 'ion' in st.session_state:
-    with st.sidebar:
-        st.image(st.session_state.ion)
